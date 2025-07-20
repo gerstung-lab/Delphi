e17584f9e01a1160d9bfa5781399d9a0ccca4038
shawnfan19 <shawnclarkefan@gmail.com>
2025-07-20 12:08:36 +0200

    deregister BLOOD_ALL

--- delphi/multimodal.py
+++ delphi/multimodal.py
@@ -4,26 +4,25 @@ from enum import Enum
 class Modality(Enum):
     # 0 for padding; 1 for event tokens
     PRS = 2
-    BLOOD_ALL = 3
-    WBC = 4
-    LIPID = 5
-    LFT = 6
-    RENAL = 7
-    HBA1C = 8
-    CRP = 9
-    URATE = 10
-    CYSC = 11
-    APO = 12
-    VITD = 13
-    DHT = 14
-    SHBG = 15
-    IGF1 = 16
-    NAK = 17
-    CREAT = 18
-    ALBU = 19
-    DIET = 20
-    MET = 21
-    TELOMERE = 22
+    WBC = 3
+    LIPID = 4
+    LFT = 5
+    RENAL = 6
+    HBA1C = 7
+    CRP = 8
+    URATE = 9
+    CYSC = 10
+    APO = 11
+    VITD = 12
+    DHT = 13
+    SHBG = 14
+    IGF1 = 15
+    NAK = 16
+    CREAT = 17
+    ALBU = 18
+    DIET = 19
+    MET = 20
+    TELOMERE = 21
 
 
 def module_name(modality: Modality) -> str:
