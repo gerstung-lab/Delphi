bfeb934e7bfc104be48410aeeb6e0e1859c3c69d
shawnfan19 <shawnclarkefan@gmail.com>
2025-07-20 09:40:49 +0200

    deregister discrete biomarker modalities
    remove associated tests

--- delphi/multimodal.py
+++ delphi/multimodal.py
@@ -4,28 +4,26 @@ from enum import Enum
 class Modality(Enum):
     # 0 for padding; 1 for event tokens
     PRS = 2
-    FAMILY_HX = 3
-    BLOOD_ALL = 4
-    WBC = 5
-    LIPID = 6
-    LFT = 7
-    RENAL = 8
-    HBA1C = 9
-    CRP = 10
-    URATE = 11
-    CYSC = 12
-    APO = 13
-    VITD = 14
-    DHT = 15
-    SHBG = 16
-    IGF1 = 17
-    NAK = 18
-    CREAT = 19
-    ALBU = 20
-    MEDS = 21
-    DIET = 22
-    MET = 23
-    TELOMERE = 24
+    BLOOD_ALL = 3
+    WBC = 4
+    LIPID = 5
+    LFT = 6
+    RENAL = 7
+    HBA1C = 8
+    CRP = 9
+    URATE = 10
+    CYSC = 11
+    APO = 12
+    VITD = 13
+    DHT = 14
+    SHBG = 15
+    IGF1 = 16
+    NAK = 17
+    CREAT = 18
+    ALBU = 19
+    DIET = 20
+    MET = 21
+    TELOMERE = 22
 
 
 def module_name(modality: Modality) -> str:
--- delphi/test/test_biomarkers.py
+++ delphi/test/test_biomarkers.py
@@ -11,7 +11,6 @@ from delphi.multimodal import Modality
 
 biomarker_path = os.path.join(DELPHI_DATA_DIR, "ukb_real_data", "biomarkers")
 all_biomarkers = [modality.lower() for modality in Modality.__members__]
-discrete_biomarkers = [Modality.FAMILY_HX.name.lower(), Modality.MEDS.name.lower()]
 
 
 def biomarkers_have_all_participants(p2i: pd.DataFrame, pids: np.ndarray) -> bool:
@@ -86,30 +85,3 @@ def test_biomarkers(biomarker_path):
     assert no_duplicate_start_pos(p2i=p2i)
     assert placeholder_time_where_no_data(p2i=p2i)
     assert real_time_where_data_exists(p2i=p2i)
-
-
-def all_tokens_valid(tokenizer: dict, data: np.ndarray) -> bool:
-
-    valid_tokens = set(tokenizer.values())
-    max_token = max(valid_tokens)
-    min_token = min(valid_tokens)
-    if min_token <= 0:
-        return False
-
-    return (np.max(data) <= max_token) and (np.min(data) >= min_token)
-
-
-@pytest.mark.parametrize(
-    "biomarker_path",
-    [os.path.join(biomarker_path, biomarker) for biomarker in discrete_biomarkers],
-)
-def test_discrete_biomarkers(biomarker_path):
-
-    with open(os.path.join(biomarker_path, "tokenizer.yaml"), "rb") as f:
-        tokenizer = yaml.safe_load(f)
-
-    data = np.load(
-        os.path.join(biomarker_path, "data.npy"), allow_pickle=True, mmap_mode="r"
-    )
-
-    assert all_tokens_valid(tokenizer=tokenizer, data=data)
