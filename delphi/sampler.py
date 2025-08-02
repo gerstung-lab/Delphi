b0b3b3624e90b516ccf077ac1c8b3828daf5acc1
shawnfan19 <shawnclarkefan@gmail.com>
2025-08-02 21:31:56 +0200

    CausalSampler -> generate()

--- delphi/sampler.py
+++ delphi/sampler.py
@@ -6,8 +6,6 @@ import torch.nn.functional as F
 
 from delphi import DAYS_PER_YEAR
 from delphi.eval import clock
-from delphi.model.transformer import Delphi
-from delphi.tokenizer import Tokenizer
 
 
 def truncate_top_k(logits: torch.Tensor, k: int) -> torch.Tensor:
@@ -92,145 +90,53 @@ class CausalSamplerConfig:
     top_k: Optional[int] = None
     temperature: float = 1.0
     max_age_in_years: float = 80
-    max_new_tokens: int = 64
-    max_total_tokens: int = 128
     termination_tokens: list[str] = field(default_factory=list)
-    zero_inflate: bool = False
-    simulate_comorbid: bool = True
-    comorbid_cutoff: float = 0.2
-    always_single_tokens: list[str] = field(default_factory=list)
 
 
-class CausalSampler:
+@clock
+@torch.no_grad()
+def generate(
+    model: torch.nn.Module,
+    idx: torch.Tensor,
+    age: torch.Tensor,
+    addon: list,
+    seed: int,
+    max_age: float,  # in days
+    termination_tokens: torch.Tensor,
+    no_repeat: bool = True,
+    top_k: Optional[int] = None,
+    temperature: float = 1.0,
+):
+    """
+    Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+    the sequence max_new_tokens times, feeding the predictions back into the model each time.
+    Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+    """
 
-    def __init__(self, cfg: CausalSamplerConfig, model: Delphi, tokenizer: Tokenizer):
+    torch.manual_seed(seed)
 
-        self.cfg = cfg
-        self.model = model
-        self.tokenizer = tokenizer
-        self.always_single_tokens = [
-            self.tokenizer[disease] for disease in self.cfg.always_single_tokens
-        ]
+    while True:
 
-        self.validate_config()
-
-        torch.manual_seed(cfg.seed)
-        torch.cuda.manual_seed(cfg.seed)
-
-    def validate_config(self):
-        assert not (
-            self.cfg.simulate_comorbid & self.cfg.zero_inflate
-        ), "simulate_comorbid and zero_inflate cannot be both True"
-
-        if self.cfg.zero_inflate:
-            assert (
-                self.model.config.loss.zero_inflate
-            ), "zero_inflate is set to True in the config, \
-                    but the model was not trained with zero inflation"
-
-    @torch.no_grad()
-    def next_token(
-        self,
-        idx: torch.Tensor,
-        age: torch.Tensor,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-
-        logits, _, _ = self.model(idx, age)
-        logits_og = logits.clone()
-
-        logits = logits[:, -1, :] / self.cfg.temperature
-
-        logits[:, self.model.config.ignore_tokens] = -torch.inf
-
-        if self.cfg.top_k is not None:
-            logits = truncate_top_k(logits, self.cfg.top_k)
-
-        if self.cfg.no_repeat:
-            fill = idx + 0
-            fill[fill == 1] = 0
-            logits = logits.scatter_(1, fill, -torch.inf)
-
-        if self.cfg.simulate_comorbid:
-            idx_next, time_til_next = sample_comorbid_based_on_cutoff(
-                logits=logits,
-                comorbid_cutoff=self.cfg.comorbid_cutoff,
-                always_single_tokens=self.always_single_tokens,
-            )
-        elif self.cfg.zero_inflate:
-            pi = self.model.pi_head(logits_og[:, -1, :])
-            idx_next, time_til_next = sample_zero_inflated_exponentials(
-                logits=logits,
-                pi=pi,
-                always_single_tokens=self.always_single_tokens,
-            )
-        else:
-            idx_next, time_til_next = sample_competing_exponentials(logits)
-
-        return idx_next, time_til_next
-
-    @clock
-    @torch.no_grad()
-    def generate(
-        self,
-        idx: torch.Tensor,
-        age: torch.Tensor,
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
-        the sequence max_new_tokens times, feeding the predictions back into the model each time.
-        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
-        """
-
-        termination_tokens = (
-            torch.Tensor(
-                [self.tokenizer[token] for token in self.cfg.termination_tokens]
-            )
-            .to(idx.device)
-            .long()
+        idx, age, *addon = model.next_token(
+            idx, age, *addon, no_repeat=no_repeat, top_k=top_k, temperature=temperature
         )
 
-        max_age = self.cfg.max_age_in_years * DAYS_PER_YEAR
-        n_new = 0
-        n_total = 0
-        while True:
+        is_termination_token = torch.isin(idx, termination_tokens)
+        if torch.logical_or(
+            is_termination_token.any(-1), (age[:, -1] > max_age).any(-1)
+        ).all():
+            break
 
-            idx_next, time_til_next = self.next_token(
-                idx=idx,
-                age=age,
-            )
-            age_next = age[..., [-1]] + time_til_next
-            age_next[time_til_next == -10000] = -10000
+    exceed_max_age = age > max_age
+    # mask all tokens after the *second* occurrence of a termination token
+    beyond_termination = (
+        torch.cumsum(torch.cumsum(is_termination_token.int(), 1), 1) > 1
+    )
 
-            n_new += idx_next.shape[1]
-            too_many_new = n_new > self.cfg.max_new_tokens
-            n_total = idx_next.shape[1] + idx.shape[1]
-            too_many_total = n_total > self.cfg.max_total_tokens
-            if too_many_new or too_many_total:
-                break
+    pad = exceed_max_age | beyond_termination
+    idx[pad] = 0
+    age[pad] = -10000
 
-            idx = torch.cat((idx, idx_next), dim=1)
-            age = torch.cat((age, age_next), dim=1)
+    logits, _, _ = model(idx, age, *addon)
 
-            sort_by_time = age.argsort(1)
-            idx = idx.gather(1, sort_by_time)
-            age = age.gather(1, sort_by_time)
-
-            is_termination_token = torch.isin(idx, termination_tokens)
-            if torch.logical_or(
-                is_termination_token.any(-1), (age_next > max_age).any(-1)
-            ).all():
-                break
-
-        exceed_max_age = age > max_age
-        # mask all tokens after the *second* occurrence of a termination token
-        beyond_termination = (
-            torch.cumsum(torch.cumsum(is_termination_token.int(), 1), 1) > 1
-        )
-
-        pad = exceed_max_age | beyond_termination
-        idx[pad] = 0
-        age[pad] = -10000
-
-        logits, _, _ = self.model(idx, age)
-
-        return idx, age, logits
+    return idx, age, logits
