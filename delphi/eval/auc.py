d9b52c85122b83c04c8e6c040b77110b9c589f93
shawnfan19 <shawnclarkefan@gmail.com>
2025-08-06 10:54:51 +0200

    tie breaking auc

--- delphi/eval/auc.py
+++ delphi/eval/auc.py
@@ -55,7 +55,16 @@ def mann_whitney_auc(x1: np.ndarray, x2: np.ndarray) -> float:
 
     n1 = len(x1)
     n2 = len(x2)
-    R1 = np.concatenate([x1, x2]).argsort().argsort()[:n1].sum() + n1
+    x12 = np.concatenate([x1, x2])
+    ranks = x12.argsort().argsort() + 1  # index starts at 1
+
+    # tie breaking
+    x_uniq, c_uniq = np.unique(x12, return_counts=True)
+    tie_vals = x_uniq[c_uniq > 1]
+    for tie_val in tie_vals:
+        ranks[x12 == tie_val] = ranks[x12 == tie_val].mean()
+
+    R1 = ranks[:n1].sum()
     U1 = n1 * n2 + 0.5 * n1 * (n1 + 1) - R1
     if n1 == 0 or n2 == 0:
         return np.nan
--- delphi/eval/test_auc.py
+++ delphi/eval/test_auc.py
@@ -2,7 +2,25 @@ import numpy as np
 
 from delphi.eval.auc import mann_whitney_auc
 
-x1 = np.array([11, 12, 13])
-x2 = np.array([1, 2])
 
-mann_whitney_auc(x1=x1, x2=x2)
+def ordinal_mann_whitney_auc(x1: np.ndarray, x2: np.ndarray) -> float:
+
+    n1 = len(x1)
+    n2 = len(x2)
+    R1 = np.concatenate([x1, x2]).argsort().argsort()[:n1].sum() + n1
+    U1 = n1 * n2 + 0.5 * n1 * (n1 + 1) - R1
+    if n1 == 0 or n2 == 0:
+        return np.nan
+    return U1 / n1 / n2
+
+
+def test_mann_whitney_auc():
+
+    rng = np.random.default_rng()
+    x12 = rng.choice(20, size=10, replace=False)
+    x1, x2 = x12[:7], x12[7:]
+
+    old_auc = ordinal_mann_whitney_auc(x1=x1, x2=x2)
+    new_auc = mann_whitney_auc(x1=x1, x2=x2)
+
+    assert old_auc == new_auc
