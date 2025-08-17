9faa20b13f4513a6130e63d12195ff22efcfe779
shawnfan19 <shawnclarkefan@gmail.com>
2025-08-17 20:58:28 +0200

    tidy ukb forecast

--- delphi/eval/ukb/forecast.py
+++ delphi/eval/ukb/forecast.py
@@ -14,7 +14,6 @@ from delphi.data.utils import duplicate_participants, eval_iter, move_batch_to_d
 from delphi.eval import eval_task
 from delphi.eval.ukb.auc import mann_whitney_auc
 from delphi.experiment.train import load_ckpt
-from delphi.model.config import parse_token_list
 from delphi.model.delphi import integrate_risk
 from delphi.sampler import generate
 
@@ -32,7 +31,6 @@ class ForecastArgs:
     no_repeat: bool = True
     top_k: Optional[int] = None
     temperature: float = 1.0
-    termination_tokens: list[str] = field(default_factory=list)
 
 
 @eval_task.register
@@ -62,10 +60,9 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
         data_loader, total=math.ceil(n_participants / n_persons_per_batch), leave=True
     )
 
-    termination_tokens = parse_token_list(task_args.termination_tokens)
-    termination_tokens = torch.Tensor(tokenizer.encode(termination_tokens)).to(device)
-
-    future_risks = list()
+    forecast_risks = list()
+    sampling_risks = list()
+    baseline_risks = list()
     labels = list()
 
     with torch.no_grad():
@@ -73,13 +70,19 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
 
             batch_input = move_batch_to_device(batch_input, device=device)
             prompt_idx, prompt_age, target_idx, target_age = batch_input
+            n_sample = task_args.n_samples
+            n_person = prompt_idx.shape[0]
 
             prompt_idx, prompt_age = duplicate_participants(
                 [prompt_idx, prompt_age], n_repeat=task_args.n_samples
             )
             prompt_logits, _ = model(prompt_idx, prompt_age)
+            baseline_logits = prompt_logits[:, -1, :]
+            baseline_logits = baseline_logits.reshape((n_person, n_sample, -1))
+            baseline_logits = torch.mean(baseline_logits, dim=-2, keepdim=False)
+            baseline_risks.append(baseline_logits.detach().cpu().numpy())
 
-            idx, age, logits = generate(
+            gen_idx, gen_age, gen_logits = generate(
                 model=model,
                 idx=prompt_idx,
                 age=prompt_age,
@@ -88,11 +91,11 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
                 top_k=task_args.top_k,
                 temperature=task_args.temperature,
                 max_age=end_age,
-                termination_tokens=termination_tokens,
+                termination_tokens=torch.tensor([tokenizer["death"]], device=device),
             )
-            idx = torch.cat((prompt_idx, idx), dim=1)
-            age = torch.cat((prompt_age, age), dim=1)
-            logits = torch.cat((prompt_logits, logits), dim=1)
+            idx = torch.cat((prompt_idx, gen_idx), dim=1)
+            age = torch.cat((prompt_age, gen_age), dim=1)
+            logits = torch.cat((prompt_logits, gen_logits), dim=1)
             sort_by_age = torch.argsort(age, dim=1)
             idx = torch.take_along_dim(input=idx, indices=sort_by_age, dim=1)
             age = torch.take_along_dim(input=age, indices=sort_by_age, dim=1)
@@ -100,34 +103,28 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
                 input=logits, indices=sort_by_age.unsqueeze(-1), dim=1
             )
 
-            B, L, V = logits.shape
-            n_sample = task_args.n_samples
-            n_person = int(B / n_sample)
-
-            if model.model_type == "delphi":
+            if model.model_type == "delphi" or model.model_type == "ethos":
                 batch_risks = integrate_risk(
                     log_lambda=logits,
                     age=age,
                     start=start_age,
                     end=end_age,
                 )
-                batch_risks = batch_risks.reshape(-1, n_sample, V)
+                batch_risks = batch_risks.reshape(n_person, n_sample, -1)
                 batch_risks = torch.nanmean(batch_risks, dim=-2, keepdim=False)
-                future_risks.append(batch_risks.detach().cpu().numpy())
-            elif model.model_type == "ethos":
-                tokens_aft_prompt = idx.clone()
-                tokens_aft_prompt[age > prompt_age] = 0
-                tokens_aft_prompt = tokens_aft_prompt.reshape((n_person, n_sample, L))
+                forecast_risks.append(batch_risks.detach().cpu().numpy())
+
+                gen_idx = gen_idx.reshape((n_person, n_sample, -1))
                 occur = torch.zeros(
-                    (n_person, n_sample, V), device=logits.device
+                    (n_person, n_sample, batch_risks.shape[-1]), device=logits.device
                 ).long()
                 occur = occur.scatter_(
                     dim=-1,
-                    index=tokens_aft_prompt,
-                    src=torch.ones_like(tokens_aft_prompt),
+                    index=gen_idx,
+                    src=torch.ones_like(gen_idx),
                 )
-                batch_risks = torch.mean(occur.float(), dim=-2, keepdim=False)
-                future_risks.append(batch_risks.detach().cpu().numpy())
+                frac_occur = torch.mean(occur.float(), dim=-2, keepdim=False)
+                sampling_risks.append(frac_occur.detach().cpu().numpy())
             elif model.model_type == "motor":
                 raise NotImplementedError
             else:
@@ -153,21 +150,29 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
             labels.append(occur_during.detach().cpu().numpy())
 
     labels = np.vstack(labels)
-    future_risks = np.vstack(future_risks)
+    forecast_risks = np.vstack(forecast_risks)
+    sampling_risks = np.vstack(sampling_risks)
+    baseline_risks = np.vstack(baseline_risks)
 
     logbook = {}
     for i in range(2, labels.shape[1]):
 
         is_ctl = labels[:, i] == 0
         is_dis = labels[:, i] == 1
-        ctl = future_risks[is_ctl, i]
-        dis = future_risks[is_dis, i]
 
         disease = tokenizer.decode(i)
         logbook[disease] = {
             "n_ctl": int(is_ctl.sum()),
             "n_dis": int(is_dis.sum()),
-            "auc": mann_whitney_auc(x1=ctl, x2=dis),
+            "forecast_auc": mann_whitney_auc(
+                x1=forecast_risks[is_ctl, i], x2=forecast_risks[is_dis, i]
+            ),
+            "sampling_auc": mann_whitney_auc(
+                x1=sampling_risks[is_ctl, i], x2=sampling_risks[is_dis, i]
+            ),
+            "baseline_auc": mann_whitney_auc(
+                x1=baseline_risks[is_ctl, i], x2=baseline_risks[is_dis, i]
+            ),
         }
 
     with open(Path(ckpt) / f"{task_name}.json", "w") as f:
