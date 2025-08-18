5461f89105fbe00a031febb3f2df026cb11995cd
shawnfan19 <shawnclarkefan@gmail.com>
2025-08-18 19:43:11 +0200

    calibrate forecast risks

--- delphi/eval/ukb/forecast.py
+++ delphi/eval/ukb/forecast.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from tqdm import tqdm
 
 from delphi import DAYS_PER_YEAR
@@ -25,6 +26,7 @@ class ForecastArgs:
     start_age_years: int = 50
     end_age_years: float = 80
     resolution_years: int = 1
+    n_calibration_bins: int = 5
     subsample: Optional[int] = None
     batch_size: int = 128
     device: str = "cuda"
@@ -32,6 +34,27 @@ class ForecastArgs:
     no_repeat: bool = True
     top_k: Optional[int] = None
     temperature: float = 1.0
+
+
+def calibrate(rates: np.ndarray, occur: np.ndarray, n_bins: int):
+
+    n = occur.shape[0]
+    q = np.linspace(0, 1, n_bins + 1)
+    rate_bins = np.quantile(rates, q=q).tolist()
+
+    rate_per_bin, incidence_per_bin, n_per_bin = list(), list(), list()
+    for i in range(n_bins):
+        in_bin = np.logical_and(rates >= rate_bins[i], rates < rate_bins[i + 1])
+        rate_per_bin.append(float(np.nanmean(rates[in_bin])))
+        incidence_per_bin.append(float(occur[in_bin].sum() / n))
+        n_per_bin.append(float(in_bin.sum()))
+
+    return {
+        "rate_bins": rate_bins,
+        "rate_per_bin": rate_per_bin,
+        "incidence_per_bin": incidence_per_bin,
+        "n_per_bin": n_per_bin,
+    }
 
 
 @eval_task.register
@@ -80,8 +103,9 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
             prompt_logits, _ = model(prompt_idx, prompt_age)
             baseline_logits = prompt_logits[:, -1, :]
             baseline_logits = baseline_logits.reshape((n_person, n_sample, -1))
-            baseline_logits = torch.mean(baseline_logits, dim=-2, keepdim=False)
-            baseline_risks.append(baseline_logits.detach().cpu().numpy())
+            baseline_prob = F.softmax(baseline_logits, dim=-1)
+            baseline_prob = torch.mean(baseline_prob, dim=-2, keepdim=False)
+            baseline_risks.append(baseline_prob.detach().cpu().numpy())
 
             gen_idx, gen_age, gen_logits = generate(
                 model=model,
@@ -165,22 +189,45 @@ def sample_future(task_args: ForecastArgs, task_name: str, ckpt: str) -> None:
     logbook = {}
     for i in range(2, labels.shape[1]):
 
-        is_ctl = labels[:, i] == 0
-        is_dis = labels[:, i] == 1
+        disease_labels = labels[:, i]
+        exclude = np.isnan(disease_labels)
+        is_ctl = disease_labels == 0
+        is_dis = disease_labels == 1
 
         disease = tokenizer.decode(i)
         logbook[disease] = {
             "n_ctl": int(is_ctl.sum()),
             "n_dis": int(is_dis.sum()),
-            "forecast_auc": mann_whitney_auc(
-                x1=forecast_risks[is_ctl, i], x2=forecast_risks[is_dis, i]
-            ),
-            "sampling_auc": mann_whitney_auc(
-                x1=sampling_risks[is_ctl, i], x2=sampling_risks[is_dis, i]
-            ),
-            "baseline_auc": mann_whitney_auc(
-                x1=baseline_risks[is_ctl, i], x2=baseline_risks[is_dis, i]
-            ),
+            "forecast": {
+                "auc": mann_whitney_auc(
+                    x1=forecast_risks[is_ctl, i], x2=forecast_risks[is_dis, i]
+                ),
+                "calibrate": calibrate(
+                    forecast_risks[~exclude, i],
+                    disease_labels[~exclude],
+                    n_bins=task_args.n_calibration_bins,
+                ),
+            },
+            "sampling_auc": {
+                "auc": mann_whitney_auc(
+                    x1=sampling_risks[is_ctl, i], x2=sampling_risks[is_dis, i]
+                ),
+                "calibrate": calibrate(
+                    sampling_risks[~exclude, i],
+                    disease_labels[~exclude],
+                    n_bins=task_args.n_calibration_bins,
+                ),
+            },
+            "baseline_auc": {
+                "auc": mann_whitney_auc(
+                    x1=baseline_risks[is_ctl, i], x2=baseline_risks[is_dis, i]
+                ),
+                "calibrate": calibrate(
+                    baseline_risks[~exclude, i],
+                    disease_labels[~exclude],
+                    n_bins=task_args.n_calibration_bins,
+                ),
+            },
         }
 
     with open(Path(ckpt) / f"{task_name}.json", "w") as f:
