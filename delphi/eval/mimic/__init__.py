d12fe75d69e018148ebf5a732ffbd38bd03c3574
shawnfan19 <shawnclarkefan@gmail.com>
2025-08-22 10:26:22 +0200

    new mimic eval tasks

--- delphi/eval/mimic/__init__.py
+++ delphi/eval/mimic/__init__.py
@@ -1,2 +1,3 @@
 from .drg_classification import DRGClassificationArgs
 from .forecast import ForecastArgs
+from .sofa import SofaArgs
--- delphi/eval/mimic/sofa.py
+++ delphi/eval/mimic/sofa.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from sklearn.metrics import top_k_accuracy_score
 from tqdm import tqdm
 
-from delphi.data.mimic import DrgPredictionDataset
+from delphi.data.mimic import SofaPredictionDataset
 from delphi.data.utils import eval_iter, move_batch_to_device
 from delphi.env import DELPHI_DATA_DIR
 from delphi.eval import eval_task
@@ -16,34 +16,26 @@ from delphi.experiment.train import load_ckpt
 
 
 @dataclass
-class DRGClassificationArgs:
+class SofaArgs:
     subsample: Optional[int] = None
     batch_size: int = 128
     device: str = "cuda"
 
 
 @eval_task.register
-def drg_classification(
-    task_args: DRGClassificationArgs, task_name: str, ckpt: str
-) -> None:
+def drg_classification(task_args: SofaArgs, task_name: str, ckpt: str) -> None:
 
     device = task_args.device
     model, _, _ = load_ckpt(ckpt)
     model.to(device)
     model.eval()
 
-    eval_ds = DrgPredictionDataset(
+    eval_ds = SofaPredictionDataset(
         input_dir=Path(DELPHI_DATA_DIR) / "mimic" / "test",
         n_positions=model.config.block_size,
         sep_time_tokens=(model.model_type != "ethos"),
     )
-    non_drg_tokens = [0, 1]
-    drg_tokens = list()
-    for token_name, token in eval_ds.vocab.stoi.items():
-        if not "DRG//" in token_name:
-            non_drg_tokens.append(token)
-        else:
-            drg_tokens.append(token)
+    quantile_tokens = eval_ds.vocab.encode(eval_ds.vocab.quantile_stokens)
 
     n_participants = (
         len(eval_ds) if task_args.subsample is None else task_args.subsample
@@ -64,14 +56,14 @@ def drg_classification(
             batch_input = move_batch_to_device(batch_input, device=device)
 
             batch_logits, _ = model(*batch_input)
-            drg_logits = batch_logits[:, -1, :]
-            drg_logits[:, non_drg_tokens] = -torch.inf
-            y_prob.append(F.softmax(drg_logits, dim=-1))
+            quantile_logits = batch_logits[:, -1, quantile_tokens]
+            y_prob.append(F.softmax(quantile_logits, dim=-1))
 
             for label in batch_label:
                 y_true.append(eval_ds.vocab.encode(label["expected"]))
 
     y_prob = torch.cat(y_prob, dim=0).detach().cpu().numpy()
-    y_prob = y_prob[:, drg_tokens]
     y_true = np.array(y_true)
-    print(top_k_accuracy_score(y_true=y_true, y_score=y_prob, k=5, labels=drg_tokens))
+    print(
+        top_k_accuracy_score(y_true=y_true, y_score=y_prob, k=5, labels=quantile_tokens)
+    )
