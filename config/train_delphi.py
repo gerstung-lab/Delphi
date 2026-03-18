6be8d31ab94aa409eb26c29e6f53e099acecd1c0
Severin Magel <116261790+sevmag@users.noreply.github.com>
2026-03-18 09:03:45 +0100

    fix typo

--- config/train_delphi.py
+++ config/train_delphi.py
@@ -4,7 +4,7 @@ import time
 out_dir = 'Delphi'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 25
-log_interval = 25 # don't print too too often
+log_interval = 25 # don't print too often
 seed = 42
 
 # we expect to overfit on this small dataset, so only save when val improves
